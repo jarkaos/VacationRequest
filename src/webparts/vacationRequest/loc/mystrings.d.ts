c7e545300855234a8d0dc2886c5e9b1d41089868
Americo <americo.perez@icloud.com>
2018-10-06 21:07:46 +0200

    Clean project

---
+++ src/webparts/vacationRequest/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IVacationRequestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'VacationRequestWebPartStrings' {
+  const strings: IVacationRequestWebPartStrings;
+  export = strings;
+}
